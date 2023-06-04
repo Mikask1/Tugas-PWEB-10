3f3ae02111fed302e3f8e6d3efefa1651655f207
Mikask <darrenprasetya40@gmail.com>
2023-06-04 20:59:19 +0700

    feat: add foto

--- download_pdf.php
+++ download_pdf.php
@@ -33,23 +33,43 @@ $pdf->AddPage('L', 'A4');
 $pdf->SetXY(10, 40);
 $pdf->SetFont('Courier', 'B', 12);
 
-$pdf->Cell(30, 10, 'NIS', 1, 0, 'C');
-$pdf->Cell(70, 10, 'Nama', 1, 0, 'C');
+$pdf->Cell(40, 10,'Foto', 1,0, 'C');
+$pdf->Cell(40, 10, 'NIS', 1, 0, 'C');
+$pdf->Cell(50, 10, 'Nama', 1, 0, 'C');
 $pdf->Cell(40, 10, 'Jenis Kelamin', 1, 0, 'C');
 $pdf->Cell(40, 10, 'Telepon', 1, 0, 'C');
-$pdf->Cell(100, 10, 'Alamat', 1, 1, 'C');
+$pdf->Cell(767, 10, 'Alamat', 1, 1, 'C');
 
 $sql = $pdo->prepare("SELECT * FROM siswa ORDER BY nis");
 $sql->execute();
 
 $pdf->SetFont('Courier', '', 12);
 while ($data = $sql->fetch()) {
-    $pdf->SetX(10);
-    $pdf->Cell(30, 10, $data['nis'], 1, 0, 'C');
-    $pdf->Cell(70, 10, $data['name'], 1, 0, 'C');
-    $pdf->Cell(40, 10, $data['jenis_kelamin'], 1, 0, 'C');
-    $pdf->Cell(40, 10, $data['telp'], 1, 0, 'C');
-    $pdf->Cell(100, 10, $data['alamat'], 1, 1, 'L');
+    $imagePath = 'uploads/'.$data['foto'];
+    if(file_exists($imagePath)){
+        list($width, $height) = getimagesize($imagePath);
+
+        $aspectRatio = $width / $height;
+        
+        $maxHeight = 30;
+        $maxWidth = 30*$aspectRatio;
+
+        $x = $pdf->GetX();
+        $y = $pdf->GetY();
+
+        $pdf->Cell(40, 40, '', 1, 0, 'C');
+        $pdf->Image($imagePath, $x+5, $y+5, 30, 30);
+
+        $pdf->SetXY($x + 40, $y);
+    }
+    else{
+        $pdf->Cell(50, 50, 'Tidak Ada Gambar', 1, 0, 'C');
+    }
+    $pdf->Cell(40, 40, $data['nis'], 1, 0, 'C');
+    $pdf->Cell(50, 40, $data['name'], 1, 0, 'C');
+    $pdf->Cell(40, 40, $data['jenis_kelamin'], 1, 0, 'C');
+    $pdf->Cell(40, 40, $data['telp'], 1, 0, 'C');
+    $pdf->Cell(67, 40, $data['alamat'], 1, 1, 'L');
 }
 
 $filename = 'Data_Siswa.pdf';
